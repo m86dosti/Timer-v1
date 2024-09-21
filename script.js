5c710ec7f5f1f9b5b3858c0bfc1e49cbfc1dd77d
𝓜𝓸𝓱𝓪𝓶𝓶𝓪𝓭 <150838661+m86dosti@users.noreply.github.com>
2024-09-21 13:38:25 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,20 @@ function updateTime() {
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
     var meridiem = "AM";
-    
     // Convert hours to 12-hour format
     if (hours > 12) {
       hours = hours - 12;
       meridiem = "PM";
     }
-  
     // Pad single digit minutes and seconds with leading zeros
     minutes = (minutes < 10 ? "0" : "") + minutes;
     seconds = (seconds < 10 ? "0" : "") + seconds;
-  
     // Display the time
     var timeString = hours + ":" + minutes + ":" + seconds + " " + meridiem;
     document.getElementById("time").innerHTML = timeString;
   }
-  
   // Update time every second
-  setInterval(updateTime, 1000);
-  
+  setInterval(updateTime, 1000);  
   // Initial call to display time immediately
   updateTime();
   
